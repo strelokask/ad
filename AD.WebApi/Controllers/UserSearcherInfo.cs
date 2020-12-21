83ced7f04ab4731bb4aebf3773b8cc743792a870
Assylzhan Shabkhatov <strelokask@gmail.com>
2020-12-21 22:19:27 +0600

    initial

---
+++ AD.WebApi/Controllers/UserSearcherInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AD.WebApi.Controllers
+{
+    public class UserSearcherInfo
+    {
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Title { get; set; }
+        public string Phone { get; set; }
+        public string Department { get; set; }
+        public string JobTitle { get; set; }
+    }
+}
